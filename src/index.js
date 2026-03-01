041738163f58c3e56141936f6a03a3c5ddfe5786
nandan kumar <nandanbharadwaj4@gmail.com>
2026-03-01 10:36:03 +0530

    Basic Notification setup done

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+async function startServer() {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            dbName: process.env.DB_NAME
+        });
+
+        console.log("MongoDB connected successfully");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error('Error starting the server:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
